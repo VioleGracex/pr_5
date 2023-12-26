9aee2f4780067e2955098aa68d99ce8b4fb1bc88
VioleGracex <highdevilxx@hotmail.com>
2023-12-26 08:54:06 +0500

    Update ToolPanel.tsx

--- src/app/Components/ToolPanel.tsx
+++ src/app/Components/ToolPanel.tsx
@@ -1,5 +1,6 @@
 // components/ToolPanel.tsx
-import React from 'react';
+"use client"; // This is a client component 👈🏽
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faArrowsAlt,
@@ -16,38 +17,76 @@ import {
   faRuler,
 } from '@fortawesome/free-solid-svg-icons';
 
+const tools = [
+  { icon: faArrowsAlt, name: "Move Tool" },
+  { icon: faSquare, name: "Marquee tool" },
+  { icon: faCrop, name: "Crop Tool" },
+  { icon: faPen, name: "Pen Tool" },
+  { icon: faPencil, name: "Pencil" },
+  { icon: faPaintBrush, name: "Brush" },
+  { icon: faSearch, name: "Zoom Tool" },
+  { icon: faFont, name: "Text Tool" },
+  { icon: faHandPaper, name: "Hand Tool" },
+  { icon: faShapes, name: "Shapes Tool" },
+  { icon: faFillDrip, name: "Bucket Tool" },
+  { icon: faRuler, name: "Show Rulers" },
+];
+
 const ToolPanel: React.FC = () => {
+  const [activeTool, setActiveTool] = useState<string | null>(null);
+  const [hoveredTool, setHoveredTool] = useState<string | null>(null);
+
+  const handleToolClick = (toolName: string) => {
+    setActiveTool(activeTool === toolName ? null : toolName);
+    // Add your toggle functionality or other logic here
+  };
+
   return (
-    <div className="w-9 ml-7 mr-7 flex flex-col items-center space-y-8 mt-6">
-      <ToolIcon icon={faArrowsAlt} name="Move Tool" />
-      <ToolIcon icon={faSquare} name="Rectangular Marquee tool" />
-      <ToolIcon icon={faCrop} name="Crop Tool" />
-      <ToolIcon icon={faPen} name="Pen Tool" />
-      <ToolIcon icon={faPencil} name="Pencil" />
-      <ToolIcon icon={faPaintBrush} name="Brush" />
-      <ToolIcon icon={faSearch} name="Zoom Tool" />
-      <ToolIcon icon={faFont} name="Text Tool" />
-      <ToolIcon icon={faHandPaper} name="Hand Tool" />
-      <ToolIcon icon={faShapes} name="Shapes Tool" />
-      <ToolIcon icon={faFillDrip} name="Bucket Tool" />
-      <ToolIcon icon={faRuler} name="Show Rulers" />
+    <div className="w-5 ml-7 mr-7 flex flex-col items-center space-y-7 mt-20">
+      {tools.map((tool, index) => (
+        <ToolIcon
+          key={index}
+          icon={tool.icon}
+          name={tool.name}
+          active={activeTool === tool.name}
+          isHovered={hoveredTool === tool.name}
+          onClick={() => handleToolClick(tool.name)}
+          onMouseEnter={() => setHoveredTool(tool.name)}
+          onMouseLeave={() => setHoveredTool(null)}
+        />
+      ))}
     </div>
   );
 };
 
-const ToolIcon: React.FC<{ icon: any; name: string }> = ({ icon, name }) => {
-    return (
-      <div className="relative w-9 ml-7 mr-7 group">
-        <FontAwesomeIcon icon={icon} className="tool-icon" />
-        <div className="opacity-0 bg-gray-800 text-white text-sm p-2 rounded-md absolute left-full ml-2 top-1/2 transform -translate-y-1/2 transition-opacity duration-300 group-hover:opacity-100">
+const ToolIcon: React.FC<{
+  icon: any;
+  name: string;
+  active: boolean;
+  isHovered: boolean;
+  onClick: () => void;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
+}> = ({ icon, name, active, isHovered, onClick, onMouseEnter, onMouseLeave }) => {
+  return (
+    <div
+      className={`relative w-12 ml-7 mr-7 space-y-2 group border ${
+        active ? 'border-blue-500 border-2 rounded ' : 'border-transparent'
+      }`}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <FontAwesomeIcon icon={icon} className="tool-icon ml-2" size="lg" />
+      {isHovered && (
+        <div
+          className={`opacity-100 bg-gray-800 text-white text-sm p-2 rounded-md absolute left-full ml-2 top-1/2 transform -translate-y-1/2 transition-opacity duration-300 whitespace-nowrap`}
+        >
           {name}
         </div>
-      </div>
-    );
-  };
-  
-  
-  
-  
+      )}
+    </div>
+  );
+};
 
 export default ToolPanel;
