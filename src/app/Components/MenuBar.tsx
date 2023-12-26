7a20645917b6a0ef6ab425d2d1e987e45d6e8636
VioleGracex <highdevilxx@hotmail.com>
2023-12-26 09:01:41 +0500

    Update MenuBar.tsx

--- src/app/Components/MenuBar.tsx
+++ src/app/Components/MenuBar.tsx
@@ -7,7 +7,8 @@ const MenuBar: React.FC = () => {
     <div className="bg-gray-800 text-white p-4 flex justify-between items-center relative z-50">
       {/* Left Side: Logo */}
       <div className="flex items-center">
-        <img src="/Logo.png" alt="Logo" className="h-8 mr-2" />
+      <img src="/Logo.png" alt="Logo" className="h-12 w-auto mr-8" />
+
         
         {/* Middle: Menu Text */}
         <div className="flex ml-auto space-x-4">
@@ -26,14 +27,41 @@ const MenuBar: React.FC = () => {
           </Menu>
 
           {/* Image Menu */}
-          {/* ... repeat the pattern for other menus ... */}
-
-          {/* Help Menu */}
-          <Menu label="Help" index={2}>
-            <MenuItem label="Help Center" />
-            <MenuItem label="Tutorials" />
-            {/* ... other help menu items ... */}
+          <Menu label="Image" index={2}>
+            <MenuItem label="Insert Image" />
+            <MenuItem label="Edit Image" />
+            {/* ... other image menu items ... */}
           </Menu>
+
+          {/* Layer Menu */}
+          <Menu label="Layer" index={3}>
+            <MenuItem label="Add Layer" />
+            <MenuItem label="Delete Layer" />
+            <MenuItem label="Merge Layers" />
+            <MenuItem label="Layer Order" />
+            {/* ... other layer menu items ... */}
+          </Menu>
+
+          {/* Select Menu */}
+          <Menu label="Select" index={4}>
+            <MenuItem label="Select Objects" />
+            <MenuItem label="Select Layer" />
+            <MenuItem label="Select All" />
+            {/* ... other select menu items ... */}
+          </Menu>
+
+          {/* View Menu */}
+          <Menu label="View" index={5}>
+            <MenuItem label="Zoom In" />
+            <MenuItem label="Zoom Out" />
+            <MenuItem label="Fit to Screen" />
+            <MenuItem label="Grid" />
+            <MenuItem label="Snap to Grid" />
+            {/* ... other view menu items ... */}
+          </Menu>
+          
+          {/* Rest of the Menus */}
+          {/* ... Add more menus and items following the same pattern ... */}
         </div>
       </div>
 
@@ -75,7 +103,7 @@ const Menu: React.FC<MenuProps> = ({ label, children, index }) => {
 
   return (
     <div className="group relative" ref={menuRef}>
-      <button className="cursor-pointer" onClick={toggleMenu}>
+      <button className="cursor-pointer whitespace-nowrap" onClick={toggleMenu}>
         {label}
       </button>
       {activeChild === index && (
@@ -93,7 +121,7 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ label }) => {
   return (
-    <button className="cursor-pointer text-left w-full text-gray-800 px-4 py-2 hover:bg-gray-300 focus:outline-none focus:bg-gray-300">
+    <button className="text-sm -cursor-pointer text-left w-full text-gray-800 px-4 py-2 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 whitespace-nowrap">
       {label}
     </button>
   );
